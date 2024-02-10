bd57c489194c8c2f4e79a6ff56073d56a8dcceb4
Samiul191139 <22303308@iubat.edu>
2024-02-11 01:16:04 +0600

    271A Beautiful Year - solved

--- 271A_Beautiful_Year.cpp
+++ 271A_Beautiful_Year.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+//#include <>
 using namespace std;
 int main()
 {
     int n;
-    while (n++)
+    cin >> n;
+    int i = true;
+    while (i == true)
     {
-        /* code */
+        n++;
+        //cout << n << endl;
+        int a,b,c,d , x;
+        x = n;
+        a = x%10;
+        x /= 10;
+        b = x%10;
+        x /= 10;
+        c = x%10;
+        x /= 10;
+        d = x%10;
+        //cout << a << " "<< b << " "<< c <<" "<< d <<endl;
+        if ((a!=b) && (b!=c) && (c!=d) && (a!=c) && (b!=d) && (a!=d))
+        {
+            i = false;
+        }
+        
     }
-    
-
+    cout << n;
     return 0;
 }
